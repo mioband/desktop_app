7d31445b512809d2510bc38da2c21e08ef8040a1
dima <d99b09@gmail.com>
2022-09-01 14:12:49 +0300

    added: Power by band id detecting

--- Mio_API_v04.py
+++ Mio_API_v04.py
@@ -209,8 +209,15 @@ class Mio_API_get_data(QRunnable):
                 while not self.stop_requested:
                     self.check_config()
                     line = self.ser.readline()
+                    decode_line = line.decode()
+                    if decode_line[0] == '%':
+                        power_s_list = decode_line.split(',')
+                        band_id = int(power_s_list[0][-1])
+                        power = int(power_s_list[1][:-2])
+                        print(f'Power of band number {band_id}: {power}%')
+                        continue
                     print(f'Data: {line}')
-                    s_list = line.decode().split(',')[:-1]
+                    s_list = decode_line.split(',')[:-1]
                     i_list = []
                     for i in s_list:
                         try:
