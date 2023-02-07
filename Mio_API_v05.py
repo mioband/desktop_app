ad4dc1e446cea2a98dfae97ede4c6f930cb131fc
dima <d99b09@gmail.com>
2023-02-07 13:34:36 +0300

    added: new parser

--- Mio_API_v05.py
+++ Mio_API_v05.py
@@ -197,76 +197,36 @@ class Mio_API_get_data(Thread):#QRunnable):
                 i_list.append(int(i))
             except:
                 pass
-        if i_list[0] == 48 or i_list[0] == 144 or i_list[0] == 80:
-            band_n = 0
-            if self.band_control.config.left_band.id == band_n:
-                if i_list[0] == 48:
-                    self.band_control.config.left_band.message['y'] = i_list[1]
-                    self.band_control.config.left_band.message['x'] = i_list[2]
-                elif i_list[0] == 144:
-                    print(i_list)
-                    self.band_control.config.left_band.message['s'] =\
-                        1 if i_list[1] > 3 else 0
-                elif i_list[0] == 80:
-                    self.band_control.config.left_band.power = i_list[1]
-                    print(f'Заряд:{i_list[1]}%')
-                decode_message = \
-                    self.band_control.config.left_band.message
-            else:
-                if i_list[0] == 48:
-                    self.band_control.config.right_band.message['y'] = i_list[1]
-                    self.band_control.config.right_band.message['x'] = i_list[2]
-                elif i_list[0] == 144:
-                    print(i_list)
-                    self.band_control.config.right_band.message['s'] =\
-                        1 if i_list[1] > 3 else 0
-                elif i_list[0] == 80:
-                    self.band_control.config.right_band.power = i_list[1]
-                    print(f'Заряд:{i_list[1]}%')
-                decode_message = \
-                    self.band_control.config.right_band.message
+        if i_list[1] == 0:
+            if i_list[0] == 48:
+                self.band_control.config.left_band.message['y'] = i_list[2]
+                self.band_control.config.left_band.message['x'] = i_list[3]
+            elif i_list[0] == 144:
+                print(i_list)
+                self.band_control.config.left_band.message['s'] = \
+                    1 if i_list[2] > 3 else 0
+            elif i_list[0] == 80:
+                self.band_control.config.left_band.power = i_list[2]
+                print(f'Заряд:{i_list[2]}%')
+
         else:
-            band_n = 1
-            if self.band_control.config.left_band.id == band_n:
-                if i_list[0] == 49:
-                    self.band_control.config.left_band.message['y'] = i_list[1]
-                    self.band_control.config.left_band.message['x'] = i_list[2]
-                elif i_list[0] == 145:
-                    print(i_list)
-                    self.band_control.config.left_band.message['s'] =\
-                        1 if i_list[1] > 3 else 0
-                elif i_list[0] == 81:
-                    self.band_control.config.left_band.power = i_list[1]
-                    print(f'Заряд:{i_list[1]}%')
-                decode_message = \
-                    self.band_control.config.left_band.message
-            else:
-                if i_list[0] == 49:
-                    self.band_control.config.right_band.message['y'] = i_list[1]
-                    self.band_control.config.right_band.message['x'] = i_list[2]
-                elif i_list[0] == 145:
-                    print(i_list)
-                    self.band_control.config.right_band.message['s'] =\
-                        1 if i_list[1] > 3 else 0
-                elif i_list[0] == 81:
-                    self.band_control.config.right_band.power = i_list[1]
-                    print(f'Заряд:{i_list[1]}%')
-                decode_message = \
-                    self.band_control.config.right_band.message
-        return decode_message, band_n
+            if i_list[0] == 48:
+                self.band_control.config.right_band.message['y'] = i_list[2]
+                self.band_control.config.right_band.message['x'] = i_list[3]
+            elif i_list[0] == 144:
+                print(i_list)
+                self.band_control.config.right_band.message['s'] = \
+                    1 if i_list[2] > 3 else 0
+            elif i_list[0] == 80:
+                self.band_control.config.right_band.power = i_list[2]
+                print(f'Заряд:{i_list[2]}%')
 
     def connect_to_band(self, band_name, hand):
         print('connect started')
-        self.cmd = bytearray(('~' + 'G' + band_name).encode('utf-8'))
+        self.cmd = bytearray(('~' + 'G' + band_name + '$' + hand).encode('utf-8'))
         self.tmp = b'GOK\r\n'
-        self.need_writing = True
-        if hand == 'right':
-            band_id = 0
-            self.band_control.config.right_band.id = band_id
-        elif hand == 'left':
-            band_id = 1
-            self.band_control.config.left_band.id = band_id
-        self.band_control.config.write('config/config.json')
+
+
 
     def band_writing(self):
         if self.need_writing:
